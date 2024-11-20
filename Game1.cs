d1a2f786fe6226307a807ae4bc0141e46c87644b
Andre Mocko <339723967@GoTVDSB.ca>
2024-11-20 09:38:30 -0500

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Monogame___Mouse_Clicks
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D quitTexture;
+        Rectangle quitRect;
+        MouseState mouseState;
+        MouseState prevMouseState;
+        Texture2D newCursorTexture;
+        Rectangle mnewCursorRect;
+
+
+
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 500;
+            quitRect = new Rectangle(300, 200, 200, 80);
+            marioMouseRect = new Rectangle();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            quitTexture = Content.Load<Texture2D>("quitTexture");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            prevMouseState = mouseState;
+            mouseState = Mouse.GetState();
+
+
+            if (mouseState.LeftButton == ButtonState.Pressed &&
+                prevMouseState.LeftButton == ButtonState.Released)
+            {
+                if (quitRect.Contains(mouseState.Position))
+                    Exit();
+
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+
+            _spriteBatch.Draw(quitTexture, quitRect, Color.White);
+            
+            
+            
+            
+            
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Monogame___Mouse_Clicks.Game1();
+game.Run();
